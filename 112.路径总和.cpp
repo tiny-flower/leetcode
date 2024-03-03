cee36a271e8099631060cc13b21c5834d2471d56
kk <mli790418@gmail.com>
2024-03-03 18:25:40 +0800

    T112

---
+++ 112.路径总和.cpp
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=112 lang=cpp
+ *
+ * [112] 路径总和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int curSum = 0;
+    bool found = false;
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        traverse(root, targetSum);
+        return found;
+    }
+    void traverse(TreeNode* root, int targetSum){
+        if(root == nullptr) return ;
+        curSum += root->val;
+        if(root->left == nullptr && root->right == nullptr){
+            if(curSum == targetSum) found = true;
+        }
+        traverse(root->left, targetSum);
+        traverse(root->right, targetSum);
+        curSum -= root->val;
+    }
+};
+// @lc code=end
+
